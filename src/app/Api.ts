048b38fc9b75d3fdc871f56617178c4319080ad4
Wrdz - Ivan Mateus <ter.imateus@restoque.com.br>
2021-02-22 15:08:57 -0300

    Att

--- src/app/Api.ts
+++ src/app/Api.ts
@@ -1,2 +1,3 @@
-export const CARS_API = 'https://spring-cars.herokuapp.com'
+//export const CARS_API = 'https://spring-cars.herokuapp.com'
 //export const CARS_API = 'http://localhost:8080'
+export const CARS_API = 'https://spring-cars.azurewebsites.net/'
